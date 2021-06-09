e4c48f4075f7827ffcecefeb8a5ad5592813f687
enzo devoto <enzodevotob@gmail.com>
2021-06-09 14:17:29 -0400

    Traffic-light finished

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function TrafficLight() {
+	const [selectedColor, setColor] = useState("false");
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 d-flex flex-column text-center">
+			<div className="palito container-fluid p-1 text-center"></div>
+			<div className="container-fluid cajaLuces text-center d-flex flex-column">
+				<div
+					onClick={() => setColor("cajaLuces_rojo")}
+					className={
+						"cajaLuces_rojo bg-danger p-5 my-4 rounded-circle" +
+						(selectedColor === "cajaLuces_rojo" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("cajaLuces_amarillo")}
+					className={
+						"cajaLuces_amarillo bg-warning p-5 m-1 rounded-circle" +
+						(selectedColor === "cajaLuces_amarillo" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("cajaLuces_verde")}
+					className={
+						"cajaLuces_verde bg-success p-5 mt-4 rounded-circle" +
+						(selectedColor === "cajaLuces_verde" ? " glow" : "")
+					}></div>
+			</div>
 		</div>
 	);
 }
